d6508ee97bb8800acd2170cfde491a13962d78c4
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-11 14:30:51 +0500

    end

--- ex25/Program.cs
+++ ex25/Program.cs
@@ -10,6 +10,32 @@ namespace ex25
     {
         static void Main(string[] args)
         {
+            int[] array = new int[5];
+            int tempValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i + 1;
+                Console.Write($"{array[i]}|");
+            }
+
+            Console.Write("\nНа сколько вы хотите сдвинуть массив влево? ");
+            int leftShiftCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < leftShiftCount; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    tempValue = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tempValue;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}|");
+            }
 
         }
     }
